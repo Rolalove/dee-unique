ade316750ae9d9dd60284f0368b1405f1613a37b
kofoworola_shonuyi <skofoworola3@gmail.com>
2025-05-26 23:51:41 +0100

    fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import Image from "next/image";
-import { Navbar } from "../components/sharedcomponent/Navbar";
 import { Whoarewe } from "../components/landingpage/Whoarewe";
 import CoreValues from "../components/landingpage/CoreValues";
-import Footer from "../components/sharedcomponent/Footer";
-import JoinDeeUniqueFamily from "../components/landingpage/JoinDeeUniqueFamily";
 import HeroSection from "@/components/landingpage/HeroSection";
-import InfoCard from "@/components/sharedcomponent/InfoCard";
 import Programs from "@/components/landingpage/Programs";
 
 export default function Home() {
